65d3b0f45e6d033c4605fbff353f7afdd32e9b54
polymatter <jlake.jobs@gmail.com>
2021-07-21 18:54:08 +0100

    try useasync hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import './App.css';
 import { init } from 'dc-extensions-sdk';
 import type { ContentFieldExtension } from 'dc-extensions-sdk';
+import { useAsync } from 'react-async';
 
-// interface FieldModel {
-//   title: string;
-//   type: string;
-//   control: string;
-//   format: string;
-//   minLength: number;
-//   maxLength: number;
-// }
 type FieldModel = number;
 interface Parameters {
   instance: {};
@@ -18,16 +11,40 @@ interface Parameters {
   }
 }
 
-const sdk = async function() {
-  return await init<ContentFieldExtension<FieldModel, Parameters>>()
-}();
+async function initialize() {
+  const sdk = await init<ContentFieldExtension<FieldModel, Parameters>>();
+  await sdk.field.setValue(3)
+  return sdk;
+};
+
+initialize();
 
 function App() {
-  return (
-    <div className="App">
-      This is a test ... {async function() { return await (await sdk).field.getValue() }()}
-    </div>
-  );
+  const { data, error, isLoading } = useAsync({ promiseFn: init as any })
+  const { data: value , error: errorValue, isLoading: isLoadingValue } = useAsync({ promiseFn: (data as ContentFieldExtension).field.getValue})
+
+  if (error) 
+    return <div className="App">Error {error}</div>
+
+  if (isLoading)
+    return <div className="App">Loading</div>
+
+  if (data) {
+    if (errorValue)
+      return <div className="App">Error Value {errorValue}</div>
+
+    if (isLoadingValue)
+      return <div className="App">Loading Value</div>
+
+    if (value)
+      return (
+        <div className="App">
+          Value is ... {value}
+        </div>
+      );
+  }
+
+  return <div className="App">Error 2</div>
 }
 
 export default App;
