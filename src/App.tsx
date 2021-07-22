57756277bddbe93ae15b23bb324c51f7fb458a44
polymatter <jlake.jobs@gmail.com>
2021-07-22 23:33:56 +0100

    fix cors issue

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,9 @@ interface Parameters {
 
 type AmplienceSdk = ContentFieldExtension<FieldModel, Parameters>;
 
-const api = (search:string) => `https://dev.poq.io/clients/demo/search/predictive?keyword=${search}`;
+const api = (search: string) => `https://dev.poq.io/clients/demo/search/predictive?keyword=${search}`;
 const headers = {
+  "Access-Control-Allow-Origin": "*",
   "Authorization": "Bearer anonymous+1fcac1b5-a3c6-4a62-80c1-62a478186863",
   "User-Agent": "com.poq.poqdemoapp-uat/20.0.1 iOS/15.0",
   "poq-app-identifier": "ca315772-4803-4b48-ae99-5683133770e6"
