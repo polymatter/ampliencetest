782a07edfda79c3d7eac8b30fb01c98d1ddba897
polymatter <jlake.jobs@gmail.com>
2021-08-27 08:12:40 +0100

    show the model

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,22 @@ import { useEffect, useState } from 'react';
 
 export default function () {
     useEffect(() => {
-        init().then(setSdk).then();
+        init().then(setSdk);
     }, [])
 
     const [sdk, setSdk] = useState<VisualizationSDK>();
+    const [stuff, setStuff] = useState<string>();
 
     if (!sdk) {
         return <div>Loading...</div>
     }
 
-    return <div>Yo my dudes SDK has loaded</div>
+    sdk.form.changed((model) => {
+        setStuff(JSON.stringify(model));
+    })
+
+    return <div>
+        <div>Yo my dudes SDK has loaded</div>
+        <div>{stuff}</div>
+    </div>
 }
