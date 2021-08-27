97b962790cdfa5cd7877aa95223c4c563546fa43
polymatter <jlake.jobs@gmail.com>
2021-08-27 18:15:42 +0100

    show banner images of slot

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
 import { init, VisualizationSDK } from 'dc-visualization-sdk';
 import { useEffect, useState } from 'react';
 
+interface AmplienceBanner {
+    '_meta': {
+        name: string,
+        schema: string,
+        deliveryId: string
+    },
+    'banner-image': {
+        '_meta': {
+            schema: string
+        },
+        id: string,
+        name: string,
+        endpoint: string,
+        defaultHost: string
+    },
+    isFeatured: boolean,
+    'banner-title': string,
+    width: number,
+    height: number,
+    actionType: string,
+    actionParameter: string,
+    paddingTop: number,
+    paddingBottom: number,
+    paddingLeft: number,
+    paddingRight: number
+}
+
+interface AmplienceSlot {
+    content: {
+        banners: AmplienceBanner[]
+        '_meta': {
+            name: string,
+            schema: string,
+            deliveryId: string
+        }
+    }
+}
+
+type AmplienceBanners = AmplienceBanner[];
+
 export default function () {
     useEffect(() => {
         init().then(setSdk);
     }, [])
 
     const [sdk, setSdk] = useState<VisualizationSDK>();
-    const [stuff, setStuff] = useState<string>();
 
-    if (!sdk) {
+    useEffect(() => {
+        if (!sdk) return;
+
+        sdk.form.get<AmplienceSlot>().then(slot => {
+            setBanners(slot.content.banners);
+        })
+    },[sdk])
+
+    const [banners, setBanners] = useState<AmplienceBanners>();
+
+    if (!sdk || !banners) {
         return <div>Loading...</div>
     }
 
-    sdk.form.changed((model) => {
-        setStuff(JSON.stringify(model));
-    })
+    sdk.form.changed((slot: AmplienceSlot) => {
+        setBanners(slot.content.banners);
+    });
+
+    const getBannerImage = (banner : AmplienceBanner) => {
+        const imageData = banner['banner-image'];
+
+        return `https://${imageData.defaultHost}/i/${imageData.endpoint}/${imageData.name}`;
+    }
 
     return <div>
         <div>Yo my dudes SDK has loaded</div>
-        <div>{stuff}</div>
+        { banners.map(banner => {
+            <img src={getBannerImage(banner)} />
+        })}
     </div>
 }
