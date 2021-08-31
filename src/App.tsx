3fc5121bc305b02e5b1a69ce7467226662a952ab
polymatter <jlake.jobs@gmail.com>
2021-08-31 14:50:31 +0100

    resize images

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,125 @@
 import { init, VisualizationSDK } from 'dc-visualization-sdk';
 import { useEffect, useState } from 'react';
+import { useWindowResize } from 'beautiful-react-hooks';
 
 interface AmplienceBanner {
+  '_meta': {
+    name: string,
+    schema: string,
+    deliveryId: string
+  },
+  'banner-image': {
     '_meta': {
-        name: string,
-        schema: string,
-        deliveryId: string
+      schema: string
     },
-    'banner-image': {
-        '_meta': {
-            schema: string
-        },
-        id: string,
-        name: string,
-        endpoint: string,
-        defaultHost: string
-    },
-    isFeatured: boolean,
-    'banner-title': string,
-    width: number,
-    height: number,
-    actionType: string,
-    actionParameter: string,
-    paddingTop: number,
-    paddingBottom: number,
-    paddingLeft: number,
-    paddingRight: number
+    id: string,
+    name: string,
+    endpoint: string,
+    defaultHost: string
+  },
+  isFeatured: boolean,
+  'banner-title': string,
+  width: number,
+  height: number,
+  actionType: string,
+  actionParameter: string,
+  paddingTop: number,
+  paddingBottom: number,
+  paddingLeft: number,
+  paddingRight: number
 }
 
 interface AmplienceSlot {
-    content: {
-        banners: AmplienceBanner[]
-        '_meta': {
-            name: string,
-            schema: string,
-            deliveryId: string
-        }
+  content: {
+    banners: AmplienceBanner[]
+    '_meta': {
+      name: string,
+      schema: string,
+      deliveryId: string
     }
+  }
 }
 
 type AmplienceBanners = AmplienceBanner[];
+type deviceRatioMap = Record<string, { index: number, platform: string, ratio: number }>
 
-export default function App() {
-    useEffect(() => {
-        init().then(setSdk);
-    }, [])
+const deviceRatios: deviceRatioMap = {
+  ios: {
+    index: 0,
+    platform: 'ios',
+    ratio: 375 / 812
+  },
+  ipad: {
+    index: 1,
+    platform: 'ios',
+    ratio: 834 / 1112
+  },
+  android: {
+    index: 2,
+    platform: 'android',
+    ratio: 360 / 740
+  }
+}
 
-    const [sdk, setSdk] = useState<VisualizationSDK>();
+export default function App({ deviceType = 'ios' }: { deviceType: keyof deviceRatioMap }) {
+  useEffect(() => {
+    init().then(setSdk);
+  }, [])
 
-    useEffect(() => {
-        if (!sdk) return;
+  const [sdk, setSdk] = useState<VisualizationSDK>();
 
-        sdk.form.get<AmplienceSlot>().then(slot => {
-            setBanners(slot.content.banners);
-        })
-    }, [sdk])
+  useEffect(() => {
+    if (!sdk) return;
 
-    const [banners, setBanners] = useState<AmplienceBanners>();
+    sdk.form.get<AmplienceSlot>().then(slot => {
+      setBanners(slot.content.banners);
+    })
+  }, [sdk])
 
-    if (!sdk || !banners) {
-        return <div>Loading...</div>
+  const [banners, setBanners] = useState<AmplienceBanners>();
+
+  if (!sdk || !banners) {
+    return <div>Loading...</div>
+  }
+
+  sdk.form.changed((slot: AmplienceSlot) => {
+    setBanners(slot.content.banners);
+  });
+
+  const getBannerImage = (banner: AmplienceBanner) => {
+    const imageData = banner['banner-image'];
+
+    return `https://${imageData.defaultHost}/i/${imageData.endpoint}/${imageData.name}`;
+  }
+
+  const [windowHeight, setHeight] = useState(window.innerHeight)
+  useWindowResize(() => {
+    setHeight(window.innerHeight);
+  })
+
+  const { ratio } = deviceRatios[deviceType];
+  const deviceWidth = 812 * ratio;
+  const deviceHeight = 812;
+
+  let scale = 1;
+  // const toolbarHeight = 134;
+  const availableHeight = windowHeight - 50;
+  if (deviceHeight > availableHeight) {
+    scale = availableHeight / deviceHeight;
+    if (scale < 0.7) {
+      scale = 0.7;
     }
+  }
 
-    sdk.form.changed((slot: AmplienceSlot) => {
-        setBanners(slot.content.banners);
-    });
-
-    const getBannerImage = (banner: AmplienceBanner) => {
-        const imageData = banner['banner-image'];
-
-        return `https://${imageData.defaultHost}/i/${imageData.endpoint}/${imageData.name}`;
-    }
-
-    return <div>
-        <div>Yo my dudes SDK has loaded</div>
-        <div>There are {banners.length} banners</div>
-        {banners.map(banner => {
-            return <img src={getBannerImage(banner)} alt={banner['banner-image'].id} />
-        })}
-    </div>
+  return <div>
+    {banners.map(banner => {
+      return <img
+        src={getBannerImage(banner)}
+        alt={banner['banner-image'].id}
+        style={{
+          width: banner.isFeatured ? deviceWidth : deviceWidth / 2,
+          height: banner.isFeatured ? deviceHeight : deviceHeight / 2
+        }} />
+    })}
+  </div>
 }
