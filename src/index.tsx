25de54158bfd45d030880b3eb96b3a3aac895385
polymatter <jlake.jobs@gmail.com>
2021-09-14 10:11:38 +0100

    deploy preview

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './PickerApp';
+import App from './BannerPreviewApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
